e1ad2abc77fdae31a9ea70d14c68ad4fe92fe256
Cauã Grigolatto <caua.grigolatto@aluno.ifsp.edu.br>
2025-07-05 20:52:42 -0300

    Add database connection factory

---
+++ myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/model/database/ConnectionFactory.java
@@ -0,0 +1,39 @@
+package br.edu.ifsp.dsw.myfinanceapi.model.database;
+
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConnectionFactory {
+	private static final Logger log = LoggerFactory.getLogger(ConnectionFactory.class);
+	
+	private static Connection conn;
+	
+	public static Connection getConnection() {
+		try {			
+			if (conn == null || conn.isClosed()) {
+				createConnection();
+			}
+		}
+		catch(Throwable t) {
+			log.error("Error on getting connection", t);
+		}
+		
+		return conn;
+	}
+	
+	private static void createConnection() throws Throwable {
+		try {
+			InitialContext context = new InitialContext();
+			DataSource ds = (DataSource) context.lookup("java:comp/env/jdbc/myfinanceapi");
+			conn = ds.getConnection();			
+		}
+		catch(Throwable t) {
+			log.error("Error on creating connection", t);
+		}
+	}
+}
