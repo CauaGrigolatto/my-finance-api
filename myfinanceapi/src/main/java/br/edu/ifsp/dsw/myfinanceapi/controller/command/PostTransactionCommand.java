2b903f7c008b34b747115b4cfd0953b730e9f2da
Cauã Grigolatto <caua.grigolatto@aluno.ifsp.edu.br>
2025-07-06 00:41:48 -0300

    Allow transaction insertion

--- myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/controller/command/PostTransactionCommand.java
+++ myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/controller/command/PostTransactionCommand.java
@@ -1,24 +1,40 @@
 package br.edu.ifsp.dsw.myfinanceapi.controller.command;
 
+import java.sql.Connection;
+
+import org.apache.http.HttpStatus;
+
+import br.edu.ifsp.dsw.myfinanceapi.model.dao.TransactionDAO;
+import br.edu.ifsp.dsw.myfinanceapi.model.database.ConnectionFactory;
 import br.edu.ifsp.dsw.myfinanceapi.model.entity.Transaction;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class PostTransactionCommand extends AbstractJsonCommand {
 	
-	public PostTransactionCommand() {
+	private TransactionDAO transactionDAO;
+	
+	public PostTransactionCommand() throws Throwable {
 		super();
+		Connection conn = ConnectionFactory.getConnection();
+		this.transactionDAO = new TransactionDAO(conn);
 	}
 	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		try {			
+		try {
 			String json = toJson(request);
 			Transaction transaction = gson.fromJson(json, Transaction.class);
+			transactionDAO.save(transaction);
 			log.info("Transaction created successfully", transaction);
+			transactionDAO.commit();
+			response.setStatus(HttpStatus.SC_CREATED);
 		}
 		catch(Throwable t) {
+			transactionDAO.rollback();
+			response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
 			log.error("Error on transaction creation", t);
+			throw t;
 		}
 	}
 }
--- myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/model/dao/TransactionDAO.java
+++ myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/model/dao/TransactionDAO.java
@@ -1,6 +1,9 @@
 package br.edu.ifsp.dsw.myfinanceapi.model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 import br.edu.ifsp.dsw.myfinanceapi.model.entity.Transaction;
@@ -13,9 +16,30 @@ public class TransactionDAO extends BasicDAO<Transaction> {
 	}
 
 	@Override
-	public void save(Transaction entity) throws Throwable {
-		// TODO Auto-generated method stub
-		
+	public void save(Transaction transaction) throws Throwable {
+		try {
+			StringBuilder sql = new StringBuilder();
+			sql.append("INSERT INTO transaction (description, value, type, due_date, category_id) VALUES ");
+			sql.append("(?,?,?,?,?) ");
+			PreparedStatement ps = conn.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, transaction.getDescription());
+			ps.setBigDecimal(2, transaction.getValue());
+			ps.setString(3, transaction.getType().toString());
+			ps.setDate(4, new java.sql.Date(transaction.getDueDate().getTime()));
+			ps.setInt(5, transaction.getCategory().getId());
+			
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			
+			if (rs.next()) {
+				Integer id = rs.getInt(1);
+				transaction.setId(id);
+			}
+		}
+		catch(Throwable t) {
+			log.error("Error on transaction saving", t); 
+			throw t;
+		}
 	}
 
 	@Override
