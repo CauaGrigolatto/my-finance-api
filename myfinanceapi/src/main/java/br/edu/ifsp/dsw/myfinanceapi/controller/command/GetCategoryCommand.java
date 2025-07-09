e17363874f61d7a56337f926cb0d7e93cd0dd8fd
Cauã Grigolatto <caua.grigolatto@aluno.ifsp.edu.br>
2025-07-08 23:20:21 -0300

    Improve response for category consult with ID

--- myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/controller/command/GetCategoryCommand.java
+++ myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/controller/command/GetCategoryCommand.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 
 import org.apache.http.HttpStatus;
 
+import br.edu.ifsp.dsw.myfinanceapi.dto.ResponseDTO;
 import br.edu.ifsp.dsw.myfinanceapi.model.dao.CategoryDAO;
 import br.edu.ifsp.dsw.myfinanceapi.model.database.ConnectionFactory;
 import br.edu.ifsp.dsw.myfinanceapi.model.entity.Category;
+import br.edu.ifsp.dsw.myfinanceapi.model.entity.Transaction;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -23,26 +25,54 @@ public class GetCategoryCommand extends AbstractJsonCommand {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		try {
-			String idStr = request.getParameter("id");
-			Integer id = Integer.parseInt(idStr);
+			String[] parts = request.getPathInfo().split("/");
+			String idStr = parts[2];
+			Integer id = Integer.valueOf(idStr);
 			
+			ResponseDTO<Category> responseDTO;
 			Category category = categoryDAO.findById(id);
 			
 			if (category == null) {
+				responseDTO = new ResponseDTO<Category>(
+					HttpStatus.SC_NOT_FOUND,
+					"Category with ID " + id + " not found.",
+					null,
+					null
+				);
+				
 				response.setStatus(HttpStatus.SC_NOT_FOUND);
-				return;
+			}
+			else {
+				responseDTO = new ResponseDTO<Category>(
+					HttpStatus.SC_OK,
+					null,
+					category,
+					null
+				);
+				
+				log.info("Category consulted successfully", category);
+				response.setStatus(HttpStatus.SC_OK);
 			}
 			
-			log.info("Category consulted successfully", category);
-			
-			String json = gson.toJson(category);
-			response.setStatus(HttpStatus.SC_OK);
+			String json = gson.toJson(responseDTO);
 			response.setContentType("application/json");
 			response.getWriter().write(json);
 		}
 		catch(Throwable t) {
+			log.error("Error on retrieving transactions");
+			
+			ResponseDTO<Transaction> responseDTO = new ResponseDTO<Transaction>(
+				HttpStatus.SC_INTERNAL_SERVER_ERROR,
+				"An error occurred while processing your request.",
+				null,
+				null
+			);
+			
+			String responnseJson = gson.toJson(responseDTO);
+			response.setContentType("application/json");
+			response.getWriter().write(responnseJson);
+			
 			response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
-			log.error("Error on finding category by ID");
 			throw t;
 		}
 		finally {			
