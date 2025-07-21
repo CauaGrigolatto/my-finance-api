de620773e2c95afe0b0f21ec4c44b219673c02bb
Cauã Grigolatto <caua.grigolatto@aluno.ifsp.edu.br>
2025-07-21 10:19:46 -0300

    Add documentation doc

--- myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/model/adapter/TransactionAdapter.java
+++ myfinanceapi/src/main/java/br/edu/ifsp/dsw/myfinanceapi/model/adapter/TransactionAdapter.java
@@ -9,7 +9,6 @@ import java.util.Date;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
@@ -29,21 +28,33 @@ public class TransactionAdapter extends TypeAdapter<Transaction> {
 	@Override
 	public Transaction read(JsonReader in) throws IOException {
 		JsonObject jsonObject = JsonParser.parseReader(in).getAsJsonObject();
-		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Date dueDate;
-		
-		try {
-			dueDate = dateFormat.parse(jsonObject.get("dueDate").getAsString());
-		} catch (ParseException e) {
-			throw new JsonParseException("Invalid date format", e);
-		}
-
+				
 		Transaction transaction = new Transaction();
 		transaction.setDescription(jsonObject.get("description").getAsString());
-		transaction.setValue(new BigDecimal(jsonObject.get("value").getAsString()));
-		transaction.setType(TransactionType.valueOf(jsonObject.get("type").getAsString()));
-		transaction.setDueDate(dueDate);
+		
+		try {			
+			transaction.setValue(new BigDecimal(jsonObject.get("value").getAsString()));
+		}
+		catch(Exception e) {
+			transaction.setValue(null);
+		}
+		
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");	
+			Date dueDate = dateFormat.parse(jsonObject.get("dueDate").getAsString());
+			transaction.setDueDate(dueDate);
+		} catch (ParseException e) {
+			transaction.setDueDate(null);
+		}
+		
+		if (jsonObject.has("type") && StringUtils.isNotBlank(jsonObject.get("type").getAsString())) {
+			try {
+				transaction.setType(TransactionType.valueOf(jsonObject.get("type").getAsString()));				
+			}
+			catch(Exception e) {
+				transaction.setType(null);
+			}
+		}
 		
 		if (jsonObject.has("category") && StringUtils.isNotBlank(jsonObject.get("category").getAsString())) {
 			Category category = new Category();
